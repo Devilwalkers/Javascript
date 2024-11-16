d50900eef2234855b5fd75db7930df67656a8142
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2024-11-16 16:14:09 +0530

    Add files via upload

---
+++ 04_js/02_switchCase.js
@@ -0,0 +1,23 @@
+const month ="January";
+
+switch(month){
+    case "January":
+        console.log("January");
+        break;
+    case "Febuary":
+        console.log("Febuary");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+    case "May":
+        console.log("May");
+        break;
+    default:
+        console.log("Does not match");
+        break;
+}
+
