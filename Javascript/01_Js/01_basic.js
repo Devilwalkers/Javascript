d23d7d2e14af87d7dbd4f8445d966f9edaa734c1
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2024-11-16 16:17:39 +0530

    Delete Javascript directory

--- Javascript/01_Js/01_basic.js
+++
@@ -1,8 +0,0 @@
-const accId="000001"
-let accName="Sagar Paul"
-let accDOB="01-04-1999"
-let accPass="S8670971856@p"
-
-// accId=1 // this cannot be allowed, this const so cannot changed 
-
-console.table([accId,accName,accDOB,accPass])
