1f937466a756998f8a9ecdd9bf598e462c667eba
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2024-11-18 19:01:39 +0000

    commit

--- Javascript/05_js/01_loops.js
+++ Javascript/05_js/01_loops.js
@@ -1,5 +1,16 @@
 // for
 
-for (let i = 1; i <= 10; i = i * 2) {
-  console.log(i);
+for (let i = 1; i <= 10; i++) {
+  // console.log(i);
+}
+
+for (let i = 1; i > -10; i--) {
+  // console.log(i);
+}
+const myArray = [1, 2, 4, 5, 6, 8, 3];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+  if (myArray[i] == 4) 
+    console.log(`This is this is exist in array ${i}`);
 }
