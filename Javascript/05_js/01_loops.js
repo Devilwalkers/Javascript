0036132e64d5465d609d079776fe4fbfc2ad9912
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2024-11-19 20:49:54 +0000

    commit

--- Javascript/05_js/01_loops.js
+++ Javascript/05_js/01_loops.js
@@ -10,7 +10,25 @@ for (let i = 1; i > -10; i--) {
 const myArray = [1, 2, 4, 5, 6, 8, 3];
 
 for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
-  if (myArray[i] == 4) 
-    console.log(`This is this is exist in array ${i}`);
+  // console.log(myArray[i]);
+  if (myArray[i] == 4) {
+    // console.log(`This is this is exist in array ${i}`);
+  }
+}
+
+for (let i = 0; i < 3; i++) {
+  // console.log(`inner loop ${i}`);
+  for (let j = 0; j < 3; j++) {
+    // console.log(`outer loop ${j}`);
+  }
+}
+
+//Table printing using Loop
+
+for (let i = 1; i < 3; i++) {
+  console.log(`${i}'s Table`);
+  
+  for (let j = 1; j <= 10; j++) {
+    console.log(i, "", "*", "", j, "=", i * j);
+  }
 }
