a8c392e46e36686133121c39a08e668cca6d9932
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2024-11-28 18:49:48 +0000

    commit

---
+++ Javascript/05_js/03_HighOrder_ArrayLoops.js
@@ -0,0 +1,44 @@
+// for of loop
+
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) {
+  //   console.log(num);
+}
+
+const greeting = "Hello world";
+for (const greet of greeting) {
+  //   console.log(`each Char is ${greet}`);
+}
+
+//Maps
+
+const map = new Map();
+map.set("IN", "INDIA");
+map.set("USA", "United state of america");
+
+for (const [key, value] of map) {
+  //   console.log(key, ": ",value);
+}
+
+const myobj = {
+  game1: "MaxPayne",
+  game2: "Minecraft",
+};
+// for (const [key,value] of map) {
+//     console.log(key, ": ",value);
+//   }
+//This is not iterable
+
+const myObject = {
+  game1: "MaxPayne",
+  game2: "Minecraft",
+  game3: "Valorant",
+};
+for (const key in myObject) {
+//   console.log(`${key} Shortcut is for ${myObject[key]}`);
+}
+
