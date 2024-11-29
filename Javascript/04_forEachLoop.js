c7234f3f02e511408d11fce3ea67986891c3aebb
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2024-11-29 19:22:28 +0000

    commit

--- Javascript/04_forEachLoop.js
+++ Javascript/04_forEachLoop.js
@@ -1,1 +1,39 @@
-const programming = ["js", "rb", "py", "java", "cpp"];
+const coding = ["js", "ruby", "python", "java", "cpp", "rust"];
+
+/*
+coding.forEach(function (item) {
+  console.log(`the value of the coding array is ${item}`);
+});
+*/
+
+/*
+coding.forEach((item) => {
+  console.log(item);
+});
+*/
+function print(item) {
+  //   console.log(item);
+}
+coding.forEach(print);
+
+coding.forEach((item, index, arr) => {
+  // console.log(item,index,arr);
+});
+
+const myCoding = [
+  {
+    languageName: "C++",
+    languageFileName: "Cpp",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  },
+  {
+    languageName: "Javascript",
+    languageFileName: "js",
+  },
+];
+myCoding.forEach((item, index) => {
+  console.log(index, item.languageName);
+});
