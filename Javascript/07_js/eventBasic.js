b57cd3ac311d6511e24eebd44dbe86c73993eaa9
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2024-12-09 21:40:47 +0000

    commit

--- Javascript/07_js/eventBasic.js
+++ Javascript/07_js/eventBasic.js
@@ -2,7 +2,22 @@
 //     alert("owl clicked")
 // }
 //new approach
+// document.getElementById("owl").addEventListener(
+//   "click",
+//   function (e) {
+//     console.log(e);
+//   }.false
+// );
+// Event propogation
+document.getElementById("images").addEventListener("click", function (e) {
+    console.log("clicked inside the ul");
+  }.false);
 document.getElementById("owl").addEventListener("click", function (e) {
-  console.log(e);
-});
+  console.log("Owl clicked");
+}.false);
+
 // attachEvent
+//type, timestamp, defaultprevented
+//target, toElement, srcElement,currentTarget
+//clientX, clientY, screenX, screenY
+//altkey, ctrlkey, shiftkey, keycode
