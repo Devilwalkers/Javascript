07888d3ab5830c1020295378a3a12e28b2321d99
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2024-12-09 21:31:23 +0000

    commit

---
+++ Javascript/07_js/eventBasic.js
@@ -0,0 +1,8 @@
+// document.getElementById('owl').onclick =function(){
+//     alert("owl clicked")
+// }
+//new approach
+document.getElementById("owl").addEventListener("click", function (e) {
+  console.log(e);
+});
+// attachEvent
