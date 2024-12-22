03de94bf4cb4099a0122dde4cf6e9d8e4ef98d8f
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2024-12-22 19:24:30 +0000

    commit

--- Javascript/07_js/eventBasic.js
+++ Javascript/07_js/eventBasic.js
@@ -9,15 +9,30 @@
 //   }.false
 // );
 // Event propogation
-document.getElementById("images").addEventListener("click", function (e) {
-    console.log("clicked inside the ul");
-  }.false);
-document.getElementById("owl").addEventListener("click", function (e) {
-  console.log("Owl clicked");
-}.false);
+// document.getElementById("images").addEventListener("click", function (e) {
+//   console.log("clicked inside the ul");
+// });
+// document.getElementById("owl").addEventListener("click", function (e) {
+//   console.log("Owl clicked");
+//   e.stopPropagation();
+// });
 
+// document.getElementById("google").addEventListener("click", (e) => {
+//   e.preventDefault();
+//   console.log("clicked Google");
+//   e.stopPropagation();
+// });
 // attachEvent
 //type, timestamp, defaultprevented
 //target, toElement, srcElement,currentTarget
 //clientX, clientY, screenX, screenY
 //altkey, ctrlkey, shiftkey, keycode
+
+document.querySelector("#images").addEventListener("click", function (e) {
+  console.log(e.target.parentNode);
+  if (e.target.tagName === "IMG") {
+    console.log(e.target.id);
+    let removeIt = e.target.parentNode;
+    removeIt.remove();
+  }
+});
